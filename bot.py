01d7d6d731a0dcbc8008fd22dfa5e6854de6a510
Aaron-p-saji <65056958+Aaron-p-saji@users.noreply.github.com>
2022-07-16 20:49:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ async def start(client: pr0fess0r_99, message: Message):
     button = [[ InlineKeyboardButton("📦 Repo", callback_data = "repo"), InlineKeyboardButton("Updates 📢", url="t.me/Mo_Tech_YT") ],
               [ InlineKeyboardButton("➕️ Add Me To Your Chat ➕️", url=f"http://t.me/{approvedbot.username}?startgroup=botstart") ]]
     await client.send_message(chat_id=message.chat.id, text=f"**__Hello {message.from_user.mention} Iam Auto Approver Join Request Bot Just [Add Me To Your Group Channnl](http://t.me/{approvedbot.username}?startgroup=botstart) || Repo https://github.com/PR0FESS0R-99/Auto-Approved-Bot||**__", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
-    pr0fess0r_99.answer_callback_query("repo", text=text, show_alert=True)
+    pr0fess0r_99.answer_callback_query("repo", text="LOL", show_alert=True)
 
 @pr0fess0r_99.on_chat_join_request(filters.group & filters.chat(CHAT_ID) if CHAT_ID else filters.group)
 async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
