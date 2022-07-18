0fafa7f5b16366791780622fe47c6abfc8008f9f
Aaron-p-saji <65056958+Aaron-p-saji@users.noreply.github.com>
2022-07-18 08:57:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ pr0fess0r_99=Client(
 )
 
 CHAT_ID = [int(pr0fess0r_99) for pr0fess0r_99 in environ.get("CHAT_ID", None).split()]
-TEXT = environ.get(" {mention} Joined {title} \n 👤 User\n ├ id: `{joinid}`\n ├ first_name : {firstname}\n ├ last_name: {lastname}\n  └ username: {username}\n", "{mention} Joined {title} \n 👤 User\n ├ id: `{joinid}`\n ├ first_name : {firstname}\n ├ last_name: {lastname}\n  └ username: {username}\n")
+TEXT = environ.get(" {mention} Joined {title} \n 👤 User\n  ├ id: `{joinid}`\n  ├ **first_name** : {firstname}\n  ├ **last_name** : {lastname}\n  └ **username** : {username}\n", " {mention} Joined {title} \n 👤 User\n  ├ id: `{joinid}`\n  ├ **first_name** : {firstname}\n  ├ **last_name** : {lastname}\n  └ **username** : {username}\n")
 APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 
 @pr0fess0r_99.on_message(filters.private & filters.command(["start"]))
