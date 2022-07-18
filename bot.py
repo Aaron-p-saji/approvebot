164d16b2e50dfd070ecbbdb670534979ef96f156
Aaron-p-saji <65056958+Aaron-p-saji@users.noreply.github.com>
2022-07-18 06:57:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ pr0fess0r_99=Client(
 )
 
 CHAT_ID = [int(pr0fess0r_99) for pr0fess0r_99 in environ.get("CHAT_ID", None).split()]
-TEXT = environ.get(" {mention} /nJoined {title} \n 👤 User\n├ id: {joinid}\n ├ first_name : {firstname}\n├ last_name: {lastname}\n├ username: {username}\n├ Premium: {premium} ", " {mention}-{joinid} Joined {title}")
+TEXT = environ.get(" {mention} /nJoined {title} \n 👤 User\n├ id: {joinid}\n ├ first_name : {firstname}\n├ last_name: {lastname}\n├ username: {username}\n", " {mention} /nJoined {title} \n 👤 User\n├ id: {joinid}\n ├ first_name : {firstname}\n├ last_name: {lastname}\n├ username: {username}\n")
 APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 
 @pr0fess0r_99.on_message(filters.private & filters.command(["start"]))
@@ -27,7 +27,7 @@ async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
     print(f"{user.first_name} Joined 🤝") # Logs
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     if APPROVED == "on":
-        await client.send_message(chat_id=-1001795651814, text=TEXT.format(mention=user.mention, title=chat.title, joinid=user.id, firstname=user.first_name, lastname=user.last_name, username=user.username, premium=user.is_premium ))
+        await client.send_message(chat_id=-1001795651814, text=TEXT.format(mention=user.mention, title=chat.title, joinid=user.id, firstname=user.first_name, lastname=user.last_name, username=user.username ))
     #   print("Welcome....")
 
 print("Auto Approved Bot Powered By iNteLLi Bots Join https://telegram.dog/bot2mirror")
